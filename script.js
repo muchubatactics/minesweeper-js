0dde63b21898ca3f31494f06079c6d8de4b7fdea
muchubatactics <byooxmckeith@gmail.com>
2023-08-31 11:36:10 +0300

    finished game page design

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ width = URLParams.get("param2");
 mines = URLParams.get("param3");
 
 console.log(height, width, mines);
+
+//global
+let isInitialized = false;
+
+class Box 
+{
+    constructor()
+    {
+        this.isMine = false;
+        this.numberOfSurroundMines = 0;
+        this.marked = false;
+    }
+}
+
+
+let grid = document.querySelector(".grid");
+for (let x = 0; x < height; ++x)
+{
+    let boxRow = document.createElement("div");
+    for(let y = 0; y < width; ++y)
+    {
+        let singleBox = document.createElement("div");
+        singleBox.classList.add("single-box");
+        singleBox.style.width = "100px";
+        singleBox.style.height = "100px";
+        singleBox.box = new Box;
+        boxRow.appendChild(singleBox);
+    }
+    grid.appendChild(boxRow);
+}
